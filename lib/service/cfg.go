ab343fa28dccbe03db157212a8d4fed7fe5156a4
Andrew Lytvynov <andrew@gravitational.com>
2020-09-30 14:04:09 -0700

    Use the correct k8s port in tctl auth sign --format=kubernetes
    Previously, it used the public_addr of the proxy directly.
    Now it parses it and replaces the port with k8s port.

--- lib/service/cfg.go
+++ lib/service/cfg.go
@@ -347,12 +347,27 @@ type ProxyConfig struct {
 	Kube KubeProxyConfig
 }
 
+func (c ProxyConfig) KubeAddr() (string, error) {
+	if !c.Kube.Enabled {
+		return "", trace.NotFound("kubernetes support not enabled on this proxy")
+	}
+	if len(c.Kube.PublicAddrs) > 0 {
+		return c.Kube.PublicAddrs[0].String(), nil
+	}
+	host := "<replace this with proxy hostname>"
+	// Try to guess the hostname from the HTTP public_addr.
+	if len(c.PublicAddrs) > 0 {
+		host = c.PublicAddrs[0].Host()
+	}
+	return fmt.Sprintf("%s:%d", host, c.Kube.ListenAddr.Port(defaults.KubeProxyListenPort)), nil
+}
+
 // KubeProxyConfig specifies configuration for proxy service
 type KubeProxyConfig struct {
 	// Enabled turns kubernetes proxy role on or off for this process
 	Enabled bool
 
-	// ListenAddr is address where reverse tunnel dialers connect to
+	// ListenAddr is the address to listen on for incoming kubernetes requests.
 	ListenAddr utils.NetAddr
 
 	// KubeAPIAddr is address of kubernetes API server
